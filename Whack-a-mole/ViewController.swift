d673646c994c0ea7df32f076735753a6de9961a3
JimmyChao90196 <myname90196@gmail.com>
2023-07-26 10:55:13 +0800

    Initial Commit

---
+++ Whack-a-mole/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whack-a-mole
+//
+//  Created by JimmyChao on 2023/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
